9fd54c8afff50eb3ac4cd02179cb9200207deb1d
marlonosia <osiamarlon101@gmail.com>
2024-11-02 19:48:15 +0800

    1st push layout color white

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,13 +9,10 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -30,8 +27,9 @@ export default function RootLayout() {
     return null;
   }
 
+  // Always use the DefaultTheme (light theme)
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack initialRouteName="Login">
         <Stack.Screen name="Login" options={{ headerShown: false }} />
         <Stack.Screen name="Signup" options={{ headerShown: false }} />
